f24a8a99ae36930c4c4cc28a82dd47298857cc46
Aleksey Denisov <deoz@yandex.ru>
2021-07-26 15:53:10 +0300

    Formatting

--- src/components/TrainRecord.js
+++ src/components/TrainRecord.js
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import TrainRecordForm from './TrainRecordForm';
+import TrainRecordHistory from './TrainRecordHistory';
 
 export default function TrainRecord() {
   const clearForm = { date: '', dist: '', edit: false };
-  const [form, setForm] = useState(clearForm); 
+  const [form, setForm] = useState(clearForm);
   const [trains, setTrains] = useState([]);
 
   const handleSubmit = (event) => {
@@ -19,9 +20,9 @@ export default function TrainRecord() {
     const hasRecord = trains.findIndex((item) => item.date.getTime() === date.getTime());
     if (hasRecord !== -1) {
       if (form.edit) {
-        trains[hasRecord].dist = Number(form.dist);  
+        trains[hasRecord].dist = Number(form.dist);
       } else {
-        trains[hasRecord].dist += Number(form.dist);  
+        trains[hasRecord].dist += Number(form.dist);
       }
     } else {
       setTrains(prev => [...prev, { id: nanoid(), date, dist: Number(form.dist) }]);
@@ -30,7 +31,7 @@ export default function TrainRecord() {
   }
 
   const handleChange = ({ target }) => {
-    setForm(prevForm => ({...prevForm, [target.name]: target.value}));
+    setForm(prevForm => ({ ...prevForm, [target.name]: target.value }));
   }
 
   const handleDelete = (id) => {
@@ -42,46 +43,14 @@ export default function TrainRecord() {
   const handleEdit = (id) => {
     return () => {
       const record = trains.find((item) => item.id === id);
-      setForm({date: record.date.toLocaleDateString(), dist: record.dist, edit: true});
+      setForm({ date: record.date.toLocaleDateString(), dist: record.dist, edit: true });
     }
   }
 
-  trains.sort((a, b) => b.date.getTime() - a.date.getTime());
-
   return (
     <div className="TrainRecord">
-      {/* <form className="TrainRecord__form" onSubmit={handleSubmit}>
-        <label>Дата (ДД.ММ.ГГГГ)
-          <input className="TrainRecord__date" name="date" value={form.date} onChange={handleChange}/>
-        </label>
-        <label>Пройдено км
-          <input className="TrainRecord__dist" name="dist" value={form.dist} onChange={handleChange}/>
-        </label>
-        <button className="TrainRecord__button">OK</button>
-      </form> */}
       <TrainRecordForm form={form} onSubmit={handleSubmit} onChange={handleChange} />
-
-      <div className="TrainRecord__history">
-        <header className="TrainRecord__header">
-          <div>Дата (ДД.ММ.ГГГГ)</div>
-          <div>Пройдено км</div>
-          <div>Действия</div>
-        </header>
-        <ul className="TrainRecord__table">
-          {trains.map((item => {
-            return (
-              <li className="TrainRecord__item" key={item.id}>
-              <div>{item.date.toLocaleDateString()}</div>
-              <div>{+item.dist.toFixed(2)}</div>
-              <div>
-                <i className="material-icons" onClick={handleEdit(item.id)}>edit</i>
-                <i className="material-icons" onClick={handleDelete(item.id)}>delete</i>
-              </div>
-            </li>
-            );
-          }))}
-        </ul>
-      </div>
+      <TrainRecordHistory trains={trains} onEdit={handleEdit} onDelete={handleDelete} />
     </div>
   );
 }
--- src/components/TrainRecordForm.js
+++ src/components/TrainRecordForm.js
@@ -1,29 +1,6 @@
+import PropTypes from 'prop-types';
+
 export default function TrainRecordForm({ form, onSubmit, onChange }) {
-  // const clearForm = { date: '', dist: '', edit: false };
-  // const [form, setForm] = useState(clearForm); 
-
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   if (form.date === '' || form.dist === '') {
-  //     return;
-  //   }
-
-  //   const formatDate = form.date.split('.');
-  //   const date = new Date(formatDate[2], formatDate[1] - 1, formatDate[0]);
-
-  //   const hasRecord = trains.findIndex((item) => item.date.getTime() === date.getTime());
-  //   if (hasRecord !== -1) {
-  //     if (form.edit) {
-  //       trains[hasRecord].dist = Number(form.dist);  
-  //     } else {
-  //       trains[hasRecord].dist += Number(form.dist);  
-  //     }
-  //   } else {
-  //     setTrains(prev => [...prev, { id: nanoid(), date, dist: Number(form.dist) }]);
-  //   }
-  //   setForm(clearForm);
-  // }
-
   return (
     <form className="TrainRecordForm" onSubmit={onSubmit}>
       <label>Дата (ДД.ММ.ГГГГ)
@@ -35,4 +12,21 @@ export default function TrainRecordForm({ form, onSubmit, onChange }) {
       <button className="TrainRecordForm__button">OK</button>
     </form>
   );
+}
+
+TrainRecordForm.defaultProps = {
+  form: { date: '', dist: '', edit: false }
+};
+
+TrainRecordForm.propTypes = {
+  form: PropTypes.exact({
+    date: PropTypes.string,
+    dist: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+    ]),
+    edit: PropTypes.bool,
+  }).isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
 }
